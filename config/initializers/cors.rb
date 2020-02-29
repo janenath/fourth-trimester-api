f6d6ca3d1dfd38ce8b56352c276d763557b75f98
Jane <jane.e.nath@gmail.com>
2020-02-28 23:04:58 -0800

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'cranky-goodall-a8f89f.netlify.com', 'www.fourthtrimester.app', 'fourthtrimester.app' 
+    origins '*' 
 
     resource '*',
       headers: :any,
