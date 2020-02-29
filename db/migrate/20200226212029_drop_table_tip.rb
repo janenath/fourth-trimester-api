6a845aa3cf2874ce0a6ba4c2372fcfc80ab25247
Jane <jane.e.nath@gmail.com>
2020-02-28 19:44:11 -0800

    fix migration error

--- db/migrate/20200226212029_drop_table_tip.rb
+++ db/migrate/20200226212029_drop_table_tip.rb
@@ -1,5 +1,5 @@
-class DropTableTip < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :tips
-  end
-end
+# class DropTableTip < ActiveRecord::Migration[6.0]
+#   def change
+#     drop_table :tips
+#   end
+# end
